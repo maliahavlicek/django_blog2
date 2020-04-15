64e0241c5947730675bc9ed2713da51e085ba7b2
maliahavlicek <malia.havlicek@gmail.com>
2020-04-14 21:31:03 -0600

    fix typo for request.FILES, add imagery

--- posts/views.py
+++ posts/views.py
@@ -28,7 +28,7 @@ def create_or_edit_post(request, pk=None):
     """
     post = get_object_or_404(Post, pk=pk) if pk else None
     if request.method == "POST":
-        form = BlogPostForm(request.POST, request.Files, instance=post)
+        form = BlogPostForm(request.POST, request.FILES, instance=post)
         if form.is_valid():
             post = form.save()
             return redirect(post_detail, post.pk)
